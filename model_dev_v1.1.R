d82a909c839ac3e7893182d4cbdbcf925e61dc7c
BethSavagar <bsavagar@rvc.ac.uk>
2023-01-19 16:56:32 +0000

    experimenting with setup of population

--- model_dev_v1.1.R
+++ model_dev_v1.1.R
@@ -10,8 +10,8 @@ beta <- 1 # transmission rate, S-E (per hour)
 sigma <- 1 # rate of becoming infectious , E-I (per hour)
 gamma <- 1 # rate of becoming recovery, I-R (per hour)
 
-b <- 1 # birth rate (per week)
-
+birth <- 1 # birth rate (per week)
+N_tot <- 1700 # number of animals in population
 
 # AGE GROUPS: max age = 5y, 1 week groups, = 261 (260.714)
 Imm <- 1:17 # Imm (1-4m) = 1w-17w
@@ -32,14 +32,19 @@ mort_1 <- 0 # natural mortality rate <6M (per week)
 mort_2 <- 1 # natural mortality rate >6M  (per week)
 ppr_mort_1 <- 0 # natural mortality rate <6M (per week)
 ppr_mort_2 <- 1 # natural mortality rate >6M  (per week)
-fertility <- 1 # only animals >18M
+birth <- 1 # only animals >18M
+age_p <- c(0,0,0.2,0.3,0.5)
 
 # offtake
 demos <- data.frame(age_cat = c("Imm","You","Juv","Sub","Adu"),
                     net_off = c(off_1,off_1,off_1,off_2,off_2),
                     mort = c(mort_1,mort_1,mort_2,mort_2,mort_2),
                     ppr_mort = c(ppr_mort_1,ppr_mort_1,ppr_mort_2,ppr_mort_2,ppr_mort_2),
-                    fert = c(0,0,0,0,fertility))
+                    birth = c(0,0,0,0,birth),
+                    age_p = age_p, # proportion of population in each age group
+                    age_n = age_p*N_tot, # number of animals in age group
+                    n_weeks = c(length(Imm), length(You), length(Juv), length(Sub), length(Adu)) # number of weeks (sub-compartments) in each age group
+                    )
                 
 
 age_params <- data.frame(
@@ -54,7 +59,33 @@ age_params <- data.frame(
   mutate(immunity = ifelse(is.na(immunity) & age_cat=="Imm", 1, 
                            if_else(is.na(immunity),0, immunity))
          ) %>%
-  left_join(demos)
+  left_join(demos) %>%
+  mutate(pop_init = age_n/n_weeks)
+
+
+# initial age groups (all susceptible)
+
+
+
+#################################################################################
+#################################################################################
+## MODEL ##
+
+TimeStop_dynamics <- 52 # 1 year, weekly timestep
+TimeStop_transmission <- 24 # 1 day, hourly timestep
+
+# Demographics LOOP
+
+for(w in 1:TimeStop_dynamics){
+  
+  # immune offspring, immune compartment
+  
+  Imm_1 <- b*imm*sum(R_adult_f)
+  
+  
+  
+}
+
 
 
 
