6456fca56ad9c53a2a1ad913cef8c6ae1865d640
BethSavagar <bsavagar@rvc.ac.uk>
2023-02-06 11:56:56 +0000

    compute maternal immunity decay from data

--- mat-imm-decay.R
+++ mat-imm-decay.R
@@ -1,11 +1,15 @@
 ## maternal immunity
-# 06/02/23
+# compute decay in maternal immunity based on empirical data
 
+library(tidyverse)
+
+# Bodjo: Tested duration of maternal immunity in 112 lambs up to 150 days after birth
 bodjo <- data.frame(
   ref = "bodjo",
   day = seq(15,150,15),
   imm = c(0.92,0.893,0.84,0.705,0.32,0.053,0.045,0.045,0.018,0.018))
 
+# 1,119 dams (17.5 % ewes and 82.5 % goats) were vaccinated with anti-PPR vaccine.
 bidjeh <- data.frame(
   ref = "bidjeh",
   day = c(30,120,150,180),
@@ -23,3 +27,42 @@ mat_imm <-
                       bidjeh,
                       Markus))
 ggplot(mat_imm, aes(x=day, y = imm, col = ref))+geom_line()
+
+
+# based on results calculate weekly decline in maternal immunity, following Bodjo data
+bodjo_wk <- bodjo %>% 
+  mutate(wk = round(day/7)) %>%
+  rbind(c("bodjo", 0,1,0)) %>%
+  mutate(day=as.numeric(day),
+         imm = as.numeric(imm),
+         wk = as.numeric(wk)) %>%
+  # set imm for >4months to 0, following consensus of papers
+  mutate(imm_data = imm,
+         imm = ifelse(wk>16,0,imm_data),
+         ) %>%
+  arrange(day)
+
+imm_decay <- data.frame(
+  wk = numeric(),
+  imm = numeric()
+)
+
+for(i in 2:nrow(bodjo_wk)){
+  
+  imm_dif <- bodjo_wk[i,"imm"] - bodjo_wk[i-1,"imm"]
+  wk_dif <- bodjo_wk[i,"wk"] - bodjo_wk[i-1,"wk"] 
+  decay = imm_dif / wk_dif
+
+  imm_decay <- imm_decay %>%
+    rbind(
+      cbind(
+        "wk" = c(bodjo_wk[i-1,"wk"]:bodjo_wk[i,"wk"]),
+        "imm" = seq(bodjo_wk[i-1,"imm"],bodjo_wk[i,"imm"], by = decay)
+      )
+    )
+}
+
+imm_decay <- imm_decay %>%
+  distinct()
+# plot of empirical data - red points - from Bodjo, against computed decay - black line + points - for model.
+ggplot(imm_decay, aes(x=wk, y=imm))+geom_line()+geom_point()+geom_point(data = bodjo_wk, aes(x=wk, y = imm_data), col="red")
