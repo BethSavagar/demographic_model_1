9cb89823c114296c358e828c4dabdea7314cb945
BethSavagar <bsavagar@rvc.ac.uk>
2023-02-21 12:55:34 +0000

    update model output to summary stats

--- scripts/model_dev_v1.1.R
+++ scripts/model_dev_v1.1.R
@@ -1,4 +1,7 @@
 ## Flock Dynamics Model Code v1.1 (19/01/23)
+# 20/02/23
+# -- outputs_dev branch: switch from tracker matrix to vectors and summary stats as output (reduce computational lload)
+
 # 06/02/23 -- rudimentary model works fine.
 
 # 30/01/23 
@@ -20,66 +23,67 @@ source("scripts/set-pars.R")
 
 TimeStop_dynamics <- 5*52 # 1 year, weekly timestep for demographic component
 TimeStop_transmission <- 24 # 1 day, hourly timestep for transission component
+output <- "summary" # define output type
 
 # initial POPULATION-STATE
+
 # define SEIR vectors for male and female age groups
-Im_init_F <- rep(0,max_age_F); Im_init_M <- rep(0,max_age_M)
-S_init_F <- age_pars_F %>% pull(pop_init_F) # all population begin S
-S_init_M <- age_pars_M %>% pull(pop_init_M) # all population begin S
-E_init_F <- rep(0,max_age_F); E_init_M <- rep(0,max_age_M)
-I_init_F <- rep(0,max_age_F); I_init_M <- rep(0,max_age_M)
-R_init_F <- rep(0,max_age_F); R_init_M <- rep(0,max_age_M)
+fIm_init <- rep(0,max_age_F); mIm_init <- rep(0,max_age_M)
+fS_init <- age_pars_F %>% pull(pop_init_F) *(1-pR) # all S except already recovered
+mS_init <- age_pars_M %>% pull(pop_init_M) *(1-pR) # all S except already recovered
+fE_init <- rep(0,max_age_F); mE_init <- rep(0,max_age_M)
+fI_init <- rep(0,max_age_F); mI_init <- rep(0,max_age_M)
+fR_init <- rep(0,max_age_F); mR_init <- rep(0,max_age_M)
 
-# Sex-Age-Disease-State Tracking matrices
-# rows = age in weeks, cols = time in weeks (dynamics model)
+if(pR>0){
+  fR_init <- age_pars_F %>% pull(pop_init_F) *pR
+  mR_init <- age_pars_M %>% pull(pop_init_M) *pR
+}
 
-week_cols <- paste0(rep("w",TimeStop_dynamics), 1:TimeStop_dynamics)
+## SUMMARY STATS DF
+# create summary data frame to store summary stats for each timestep
+sum_stats <-  c("w", "sum_pop", "prop_immune", "prop_inf", "pKid", "pYou", "pJuv", "pSub", "pAdu", "pF")
+summary_df <- as.data.frame(matrix(0,
+                                   nrow=TimeStop_dynamics, 
+                                   ncol = length(sum_stats)))
+colnames(summary_df) <- sum_stats
 
-# set matrices and populate with initial population state
-# immune births
-fIm_mat <- as.data.frame(matrix(nrow = max_age_F, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = Im_init_F)
+# total population size
+fpop <- fIm_init+fS_init+fE_init+fI_init+fR_init
+mpop <- mIm_init+mS_init+mE_init+mI_init+mR_init
+mpop <- c(mpop, rep(0,max_age_F-max_age_M))
+pop_tot <- fpop+mpop
+sum_pop <- sum(fpop)+sum(mpop)
 
-mIm_mat <- as.data.frame(matrix(nrow = max_age_M, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = Im_init_M)
+# population growth (yearly calculation)
+# proportion immune
+fimmune <- fIm_init+fR_init
+mimmune <- mIm_init+mR_init
+sum_immune <- sum(fimmune)+sum(mimmune)
+prop_immune <- sum_immune / sum_pop
 
-# susceptibles
-fS_mat <- as.data.frame(matrix(nrow = max_age_F, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = S_init_F)
+# proportion infectious
+sum_inf <- sum(fI_init)+sum(mI_init)
+prop_inf <- sum_inf/sum_pop
 
-mS_mat <- as.data.frame(matrix(nrow = max_age_M, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = S_init_M)
+# proportion in each age-group
+Kid_tot <- pop_tot[Kid] ; sum_Kid <- sum(Kid_tot) ; pKid <- sum_Kid/sum_pop
+You_tot <- pop_tot[You] ; sum_You <- sum(You_tot) ; pYou <- sum_You/sum_pop
+# KidYou_tot <- Kid_tot+You_tot ; sum_KidYou <- sumKid+sumYou ; pKidYou <- sumKidYou/sum_pop
+Juv_tot <- pop_tot[Juv] ; sum_Juv <- sum(Juv_tot) ; pJuv <- sum_Juv/sum_pop
+Sub_tot <- pop_tot[Sub] ; sum_Sub <- sum(Sub_tot) ; pSub <- sum_Sub/sum_pop
+Adu_tot <- pop_tot[Adu_F] ; sum_Adu <- sum(Adu_tot) ; pAdu <- sum_Adu/sum_pop
+# also add for female and male stratification (of adults/all) ??
 
-# exposed
-fE_mat <- as.data.frame(matrix(nrow = max_age_F, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = E_init_F)
+# proportion female
+F_tot <- fIm_init+fS_init+fE_init+fI_init+fR_init
+F_sum <- sum(F_tot)
+pF <- F_sum/sum_pop
 
-mE_mat <- as.data.frame(matrix(nrow = max_age_M, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = E_init_M)
+summary_df[1,] <- c(1, sum_pop, prop_immune, prop_inf, pKid,pYou,pJuv,pSub,pAdu,pF)
 
-# infectious
-fI_mat <- as.data.frame(matrix(nrow = max_age_F, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = I_init_F)
 
-mI_mat <- as.data.frame(matrix(nrow = max_age_M, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = I_init_M)
 
-# recovered
-fR_mat <- as.data.frame(matrix(nrow = max_age_F, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = R_init_F)
-
-mR_mat <- as.data.frame(matrix(nrow = max_age_M, ncol = TimeStop_dynamics)) %>%
-  rename_all(~week_cols) %>%
-  mutate("w1" = R_init_M)
 
 ## DEMOGRAPHIC PARAMS
 # Pull demographic rates as vectors from data-frames (for vectorised equations)
@@ -102,16 +106,20 @@ birth <- age_pars_F %>% pull(birth)
 
 for(w in 2:TimeStop_dynamics){
   
-  # update week of simulation
-  w_prev <- paste0("w",w-1)
-  w_cur <- paste0("w",w)
-
+  if(w == 2){
+    fIm_prev <- fIm_init ; mIm_prev <- mIm_init
+    fS_prev <- fS_init ; mS_prev <- mS_init
+    fE_prev <- fE_init ; mE_prev <- mE_init
+    fI_prev <- fI_init ; mI_prev <- mI_init
+    fR_prev <- fR_init ; mR_prev <- mR_init
+  }
+  
   ## BIRTHS ## 
   
   # Only females > 18M can give birth (birth rate =0 for all other age groups)
   # No M/F differentiation for births
-  Im_births <- sum(birth* Imm_b* fR_mat[,w_prev]) # immune births 
-  S_births <- sum(birth*(1-Imm_b)* fR_mat[,w_prev]) + sum(birth*fS_mat[,w_prev]) # Sus births = non-immune births to R mothers, and births to S mothers.
+  Im_births <- sum(birth* Imm_b* fR_prev) # immune births 
+  S_births <- sum(birth*(1-Imm_b)* fR_prev) + sum(birth*fS_prev) # Sus births = non-immune births to R mothers, and births to S mothers.
   
   E_births <- 0; I_births <- 0; R_births <- 0 # set EIR births to 0
   
@@ -119,119 +127,145 @@ for(w in 2:TimeStop_dynamics){
   ## SEIR DEMOGRAPHICS ##
   
   # Immune Offspring Demographics (f/m)
-  fIm_new <- fIm_mat[,w_prev]*(immunity_F)*(1-net_off_F)*(1-mort_F) # decline in mat immunity, offtake and mortality
-  mIm_new <- mIm_mat[,w_prev]*(immunity_M)*(1-net_off_M)*(1-mort_M)
+  fIm_new <- fIm_prev*(immunity_F)*(1-net_off_F)*(1-mort_F) # decline in mat immunity, offtake and mortality
+  mIm_new <- mIm_prev*(immunity_M)*(1-net_off_M)*(1-mort_M)
   
-  fIm_mat[,w_cur] <- c(0.5*Im_births, fIm_new[1:max_age_F-1]) # 0.5*births for F/M, # subset demographics remove last age-group
-  mIm_mat[,w_cur] <- c(0.5*Im_births, mIm_new[1:max_age_M-1]) # 0.5*births for F/M, # subset demographics remove last age-group
+  fIm_cur <- c(0.5*Im_births, fIm_new[1:max_age_F-1]) # 0.5*births for F/M, # subset demographics remove last age-group
+  mIm_cur <- c(0.5*Im_births, mIm_new[1:max_age_M-1]) # 0.5*births for F/M, # subset demographics remove last age-group
   
   ##
   # Susceptible Demographics
-  fS_new <- fS_mat[,w_prev]*(1-net_off_F)*(1-mort_F) + # survival (those not offtake, and not died)
-    fIm_mat[,w_prev]*(1-immunity_F) # add offspring which have lost immunity
+  fS_new <- fS_prev*(1-net_off_F)*(1-mort_F) + # survival (those not offtake, and not died)
+    fIm_prev*(1-immunity_F) # add offspring which have lost immunity
   
-  mS_new <- mS_mat[,w_prev]*(1-net_off_M)*(1-mort_M) +
-    mIm_mat[,w_prev]*(1-immunity_M) # add offspring which have lost immunity
+  mS_new <- mS_prev*(1-net_off_M)*(1-mort_M) +
+    mIm_prev*(1-immunity_M) # add offspring which have lost immunity
   
   # update fS and mS matrices
-  fS_mat[,w_cur] <- c(0.5*S_births, fS_new[1:max_age_F-1]) # 0.5* births are F, lose last age group
-  mS_mat[,w_cur] <- c(0.5*S_births, mS_new[1:max_age_M-1])# 0.5* births are M, lose last age group
+  fS_cur <- c(0.5*S_births, fS_new[1:max_age_F-1]) # 0.5* births are F, lose last age group
+  mS_cur <- c(0.5*S_births, mS_new[1:max_age_M-1])# 0.5* births are M, lose last age group
   
   ##
   # Exposed Demographics:
-  fE_new <- fE_mat[,w_prev]*(1-net_off_F)*(1-mort_F) # F offtake and mortality
-  mE_new <- mE_mat[,w_prev]*(1-net_off_M)*(1-mort_M) # M offtake and mortality
+  fE_new <- fE_prev*(1-net_off_F)*(1-mort_F) # F offtake and mortality
+  mE_new <- mE_prev*(1-net_off_M)*(1-mort_M) # M offtake and mortality
   
-  fE_mat[,w_cur] <- c(0.5*E_births,fE_new[1:max_age_F-1]) # F add births (E_births = 0)  and remove last age group
-  mE_mat[,w_cur] <- c(0.5*E_births,mE_new[1:max_age_M-1]) # M add births (E_births = 0) and remove last age group
+  fE_cur <- c(0.5*E_births,fE_new[1:max_age_F-1]) # F add births (E_births = 0)  and remove last age group
+  mE_cur <- c(0.5*E_births,mE_new[1:max_age_M-1]) # M add births (E_births = 0) and remove last age group
   
   ##
   # Infectious Demographics:
-  fI_new <- fI_mat[,w_prev]*(1-net_off_F)*(1-mort_F) # F offtake and mortality (**ppr_mortality in disease loop?)
-  mI_new <- mI_mat[,w_prev]*(1-net_off_M)*(1-mort_M) # M offtake and mortality
+  fI_new <- fI_prev*(1-net_off_F)*(1-mort_F) # F offtake and mortality (**ppr_mortality in disease loop?)
+  mI_new <- mI_prev*(1-net_off_M)*(1-mort_M) # M offtake and mortality
   
-  fI_mat[,w_cur] <- c(0.5*I_births,fI_new[1:max_age_F-1]) # F add births (I_births = 0) and remove last age group
-  mI_mat[,w_cur] <- c(0.5*I_births,mI_new[1:max_age_M-1]) # M add births (I_births = 0) and remove last age group
+  fI_cur <- c(0.5*I_births,fI_new[1:max_age_F-1]) # F add births (I_births = 0) and remove last age group
+  mI_cur <- c(0.5*I_births,mI_new[1:max_age_M-1]) # M add births (I_births = 0) and remove last age group
   
   # Recovered Demographics:
-  fR_new <- fR_mat[,w_prev]*(1-net_off_F)*(1-mort_F) # offtake and mortality (**ppr_mortality in disease loop?)
-  mR_new <- mR_mat[,w_prev]*(1-net_off_M)*(1-mort_M)
+  fR_new <- fR_prev*(1-net_off_F)*(1-mort_F) # offtake and mortality (**ppr_mortality in disease loop?)
+  mR_new <- mR_prev*(1-net_off_M)*(1-mort_M)
   
-  fR_mat[,w_cur] <- c(0.5*R_births,fR_new[1:max_age_F-1]) # F add births (R_births = 0) and remove last age group
-  mR_mat[,w_cur] <- c(0.5*R_births,mR_new[1:max_age_M-1]) # M add births (R_births = 0) and remove last age group
+  fR_cur <- c(0.5*R_births,fR_new[1:max_age_F-1]) # F add births (R_births = 0) and remove last age group
+  mR_cur <- c(0.5*R_births,mR_new[1:max_age_M-1]) # M add births (R_births = 0) and remove last age group
   
-  # trnamsission loop here >>>>>
+  ## TRANSMISSION LOOP ##
   
  
-  ## OUTPUT ##
+
+  ## OUTPUT....CREATE OUTPUTS THAT DON'T DEPEND ON TRACKING MATRIX >>>>
   # reduce output so that rather than storing entire matrices we just produce summary statistics: need to decide what stats are of interest (06/02/23)
-  # totals = dataframe tracking population in each disease-state over time 
-  # sum totals for each disease compartment, and total population size.
-  totals <- data.frame(
-    "Im" = apply(fIm_mat, 2, sum)+apply(mIm_mat, 2, sum),
-    "S" = apply(fS_mat, 2, sum)+apply(mS_mat, 2, sum),
-    "E" = apply(fE_mat, 2, sum)+apply(mE_mat, 2, sum),
-    "I" = apply(fI_mat, 2, sum)+apply(mI_mat, 2, sum),
-    "R" = apply(fR_mat, 2, sum)+apply(mR_mat, 2, sum)
-  ) %>%
-    mutate("all" = apply(.,1,sum), # total population size
-           "imm_frac" = (R+Im)/all, # immune proportion of population
-           "time" = 1:TimeStop_dynamics) # timestep
   
-  ## CREATE OUTPUTS THAT DON'T DEPEND ON TRACKING MATRIX >>>>
+  # summary stats (for sensitivity analysis)
+  if(output=="summary"){
+    
+    
+    # total population size
+    fpop <- fIm_cur+fS_cur+fE_cur+fI_cur+fR_cur
+    mpop <- mIm_cur+mS_cur+mE_cur+mI_cur+mR_cur
+    mpop <- c(mpop, rep(0,max_age_F-max_age_M))
+    pop_tot <- fpop+mpop
+    sum_pop <- sum(fpop)+sum(mpop)
+    
+    # population growth (yearly calculation)
+    # proportion immune
+    fimmune <- fIm_cur+fR_cur
+    mimmune <- mIm_cur+mR_cur
+    sum_immune <- sum(fimmune)+sum(mimmune)
+    prop_immune <- sum_immune / sum_pop
+    
+    # proportion infectious
+    sum_inf <- sum(fI_cur)+sum(mI_cur)
+    prop_inf <- sum_inf/sum_pop
+    
+    # proportion in each age-group
+    Kid_tot <- pop_tot[Kid] ; sum_Kid <- sum(Kid_tot) ; pKid <- sum_Kid/sum_pop
+    You_tot <- pop_tot[You] ; sum_You <- sum(You_tot) ; pYou <- sum_You/sum_pop
+    # KidYou_tot <- Kid_tot+You_tot ; sum_KidYou <- sumKid+sumYou ; pKidYou <- sumKidYou/sum_pop
+    Juv_tot <- pop_tot[Juv] ; sum_Juv <- sum(Juv_tot) ; pJuv <- sum_Juv/sum_pop
+    Sub_tot <- pop_tot[Sub] ; sum_Sub <- sum(Sub_tot) ; pSub <- sum_Sub/sum_pop
+    Adu_tot <- pop_tot[Adu_F] ; sum_Adu <- sum(Adu_tot) ; pAdu <- sum_Adu/sum_pop
+    # also add for female and male stratification (of adults/all) ??
+    
+    # proportion female
+    F_tot <- fIm_cur+fS_cur+fE_cur+fI_cur+fR_cur
+    F_sum <- sum(F_tot)
+    pF <- F_sum/sum_pop
+    
+    summary_df[w,] <- c(w, sum_pop, prop_immune, prop_inf, pKid,pYou,pJuv,pSub,pAdu,pF)
+    
+  }else if(output=="totals"){
+    # totals <- data.frame(
+    #   "Im" = apply(fIm_mat, 2, sum)+apply(mIm_mat, 2, sum),
+    #   "S" = apply(fS_mat, 2, sum)+apply(mS_mat, 2, sum),
+    #   "E" = apply(fE_mat, 2, sum)+apply(mE_mat, 2, sum),
+    #   "I" = apply(fI_mat, 2, sum)+apply(mI_mat, 2, sum),
+    #   "R" = apply(fR_mat, 2, sum)+apply(mR_mat, 2, sum)
+    # ) %>%
+    #   mutate("all" = apply(.,1,sum), # total population size
+    #          "imm_frac" = (R+Im)/all, # immune proportion of population
+    #          "time" = 1:TimeStop_dynamics) # timestep
+  }
+
+  # update time and state vectors: 
   
-  # total population size
-  # proportion immune population
+  fIm_prev <- fIm_cur ; mIm_cur <- mIm_cur
+  fS_prev <- fS_cur ; mS_prev <- mS_cur
+  fE_prev <- fE_cur ; mE_prev <- mE_cur
+  fI_prev <- fI_cur ; mI_prev <- mI_cur
+  fR_prev <- fR_cur ; mR_prev <- mR_cur
   
 }
 
-ggplot(totals,aes(x=time,y=all))+
+summary_df <- summary_df %>%
+  mutate(w = as.numeric(w))
+
+ggplot(summary_df,aes(x=w,y=sum_pop))+
   geom_line(size = 1.5)+
   labs(title = "Total Population",
        x="time (weeks)", 
        y = "total population")+
   theme_bw()
 
-## test age-sex proportions
 
-colnames(fS_mat) <- 1:ncol(fS_mat)
-S_pop <- fS_mat %>%
-  mutate(age_gp = age_pars_F %>% pull(age_cat)) %>%
-  gather(-age_gp, key="week", value = "pop") %>%
-  mutate(week=as.numeric(week))%>%
-  group_by(age_gp, week) %>%
-  summarise(pop=sum(pop))
-
-ggplot(S_pop,aes(x=week,y=pop, group = age_gp, col = age_gp))+
-  geom_line(size = 1.5)+
-  labs(title = "FEMALE",
-       x="time (weeks)", 
-       y = "total population")+
-  theme_bw()
-
-colnames(mS_mat) <- 1:ncol(mS_mat)
-mS_pop <- mS_mat %>%
-  mutate(age_gp = age_pars_M %>% pull(age_cat)) %>%
-  gather(-age_gp, key="week", value = "pop") %>%
-  mutate(week=as.numeric(week))%>%
-  group_by(age_gp, week) %>%
-  summarise(pop=sum(pop))
-
-ggplot(mS_pop,aes(x=week,y=pop, group = age_gp, col = age_gp))+
-  geom_line(size = 1.5)+
-  labs(title = "MALE",
-       x="time (weeks)", 
-       y = "total population")+
-  theme_bw()
+## melt summary_df to long format
+summary_long <- summary_df %>%
+  gather(key="stat", value="prop", -w)
 
 
-allS_pop <- S_pop %>%
-  left_join(mS_pop, by=c("age_gp","week")) %>%
-  mutate(all_pop = pop.x+pop.y)
+## create df for different stats:
 
-ggplot(allS_pop,aes(x=week,y=all_pop, group = age_gp, col = age_gp))+
-  geom_line(size = 1.5)+
-  labs(title = "ALL (male & female)", 
-       x="time (weeks)", 
-       y = "total population")+
-  theme_bw()
+# age-sex
+summary_agesex <- summary_long %>%
+  filter(!stat %in% c("sum_pop",
+                      "prop_inf",
+                      "prop_immune"))
+
+## Plots:
+# age-sex proportions
+ggplot(summary_agesex, aes(x=w, y=prop, group=stat, col=stat))+
+  geom_line()
+
+# immunity
+ggplot(summary_df, aes(x=w, y=prop_immune))+
+  geom_line()
+
--- scripts/set-pars.R
+++ scripts/set-pars.R
@@ -8,7 +8,7 @@
 beta <- NA # transmission rate, S-E (per hour)
 sigma <- NA # rate of becoming infectious , E-I (per hour)
 gamma <- NA # rate of becoming recovery, I-R (per hour)
-
+pR <- 0.5 # proportion initially immune (due to prior infection)
 
 # Flock Structure: Sex-Age groups
 N_tot <- 1700 # number of animals in population (flock or village)?
@@ -19,7 +19,7 @@ max_age_M <- 5*52 # 3y*52w
 wk2mnth <- 4.345 # no. of weeks per month
 age_cuts_mnth <- c(4,6,12,18)
 age_cuts_wk <- round(age_cuts_mnth*wk2mnth) # age-group boundaries in months
-Imm <- 1:age_cuts_wk[1] # Immune Offspring (1-4m) = 1w-17w 
+Kid <- 1:age_cuts_wk[1] # Immune Offspring (1-4m) = 1w-17w 
 You <- (age_cuts_wk[1]+1):age_cuts_wk[2] # Young (5-6m) = 18w-26w  
 Juv <- (age_cuts_wk[2]+1):age_cuts_wk[3] # Juvenile (6-12m) = 27w-52w
 Sub <- (age_cuts_wk[3]+1):age_cuts_wk[4] # Sub-adult (12-18m) = 53w-78w
@@ -49,7 +49,7 @@ age_p_f <- c(0,0,0.15,0.15,0.5) # proportion of population in each age group (in
 age_p_m <- c(0,0,0.1,0.05,0.05)
 
 # Dataframe of demographic parameters for age-sex group
-demos <- data.frame(age_cat = c("Imm","You","Juv","Sub","Adu"), # age group
+demos <- data.frame(age_cat = c("Kid","You","Juv","Sub","Adu"), # age group
                     net_off_F = c(off_1,off_1,off_1,off_F,off_F), # female offtake
                     net_off_M = c(off_1,off_1,off_1,off_M,off_M), # male offtake
                     mort = c(mort_1,mort_1,mort_2,mort_2,mort_2), # natural mortality
@@ -57,8 +57,8 @@ demos <- data.frame(age_cat = c("Imm","You","Juv","Sub","Adu"), # age group
                     birth = c(0,0,0,0,birth_r), # birth rate (only Adu_F)
                     age_p_F = age_p_f, # proportion of population in each age group F
                     age_p_M = age_p_m, # proportion of population in each age group M
-                    n_weeks_F = c(length(Imm), length(You), length(Juv), length(Sub), length(Adu_F)), # number of weeks (sub-compartments) in each age group
-                    n_weeks_M = c(length(Imm), length(You), length(Juv), length(Sub), length(Adu_M)) # number of weeks (sub-compartments) in each age group
+                    n_weeks_F = c(length(Kid), length(You), length(Juv), length(Sub), length(Adu_F)), # number of weeks (sub-compartments) in each age group
+                    n_weeks_M = c(length(Kid), length(You), length(Juv), length(Sub), length(Adu_M)) # number of weeks (sub-compartments) in each age group
 )
 
 ## CREATE M/F DATAFRAMES ##
@@ -67,7 +67,7 @@ demos <- data.frame(age_cat = c("Imm","You","Juv","Sub","Adu"), # age group
 # rows = weeklong age gorup, cols = demographic rates 
 age_pars_F <- data.frame(
   age_weeks = 1:max_age_F, # nrow = age in weeks
-  age_cat = c(rep("Imm",length(Imm)),
+  age_cat = c(rep("Kid",length(Kid)),
               rep("You",length(You)),
               rep("Juv",length(Juv)),
               rep("Sub",length(Sub)),
@@ -76,7 +76,7 @@ age_pars_F <- data.frame(
   left_join(imm_decay_corrected, c("age_weeks" = "week_corrected")) %>%
   mutate(imm = ifelse(is.na(imm),0,imm)) %>%
   # left_join(Imm_wane, c("age_weeks" = "weeks")) %>%
-  # mutate(immunity = ifelse(is.na(immunity) & age_cat=="Imm", 1, # immunity wanes at weeks 4,8,12 (and 17) between which immunity is maintained i.e. =1
+  # mutate(immunity = ifelse(is.na(immunity) & age_cat=="Kid", 1, # immunity wanes at weeks 4,8,12 (and 17) between which immunity is maintained i.e. =1
   #                          if_else(is.na(immunity),0, immunity))) %>%
   # Join demographics (Female)
   left_join(demos, by = "age_cat") %>%
@@ -90,7 +90,7 @@ age_pars_F <- data.frame(
 # Male dataframe of demographic rates for each week-long age group in the population.
 age_pars_M <- data.frame(
   age_weeks = 1:max_age_M,
-  age_cat = c(rep("Imm",length(Imm)),
+  age_cat = c(rep("Kid",length(Kid)),
               rep("You",length(You)),
               rep("Juv",length(Juv)),
               rep("Sub",length(Sub)),
@@ -99,7 +99,7 @@ age_pars_M <- data.frame(
   left_join(imm_decay_corrected, c("age_weeks" = "week_corrected")) %>%
   mutate(imm = ifelse(is.na(imm),0,imm)) %>%
   # left_join(Imm_wane, c("age_weeks" = "weeks")) %>%
-  # mutate(immunity = ifelse(is.na(immunity) & age_cat=="Imm", 1, # immunity wanes at weeks 4,8,12 (and 17) between which immunity is maintained i.e. =1
+  # mutate(immunity = ifelse(is.na(immunity) & age_cat=="Kid", 1, # immunity wanes at weeks 4,8,12 (and 17) between which immunity is maintained i.e. =1
   #                          if_else(is.na(immunity),0, immunity))) %>%
   # Join demographics (Female)
   left_join(demos, by = "age_cat") %>%
