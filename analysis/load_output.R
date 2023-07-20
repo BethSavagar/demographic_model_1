ddf4c3129490dd9b23ef6a0de0741558ec3a9173
BethSavagar <bsavagar@rvc.ac.uk>
2023-07-20 10:56:06 +0100

    add wkly rate simulations data

--- analysis/load_output.R
+++ analysis/load_output.R
@@ -8,3 +8,7 @@ var_input_set <- read.csv("output/RSA_output/RSA_pars-set_2023-07-12.csv")
 # 100,000 parameter sets from SCC with N=100
 # RSAoutput <- read.csv("output/RSA_output/RSA_output_2023-07-17.csv")
 # var_input_set <- read.csv("output/RSA_output/RSA_pars-set_2023-07-17.csv")
+
+# 10,000 parameter sets from SCC with N=150 --> WEEKLY RATES (wkly.min.1 / wkly.max.1 - from bryony's thesis)
+# RSAoutput <- read.csv("output/RSA_output/RSA_output_2023-07-18.csv")
+# var_input_set <- read.csv("output/RSA_output/RSA_pars-set_2023-07-18.csv")
