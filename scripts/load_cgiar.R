a7b2b9c22368e4b636dd45ecba0fd5d0cf3d6186
BethSavagar <bsavagar@rvc.ac.uk>
2023-10-20 15:24:06 +0100

    tidying up files

--- scripts/load_cgiar.R
+++
@@ -1,5 +0,0 @@
-# load data for GSA
-
-fix_age_data_full <- read_csv("data/Applied_parameters/cgiar_1.csv",col_names=T)
-var_demo_data_full <- read_csv("data/Applied_parameters/cgiar_2.csv", col_names=T)
-imm_decay_corrected <- read_csv("data/imm_decay_bodjo_v2.csv") 
